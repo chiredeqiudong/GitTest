6e5b682a2ff7bb1a13cda3b9220726c495775215
itzy <hello@itzy.cn>
2024-01-27 21:37:09 +0800

    dev:2

--- src/main/java/com/it/pojo/Pojo.java
+++ src/main/java/com/it/pojo/Pojo.java
@@ -9,6 +9,7 @@ public class Pojo {
         System.out.println("master:1");
         System.out.println("master:2");
         System.out.println("dev:1");
+        System.out.println("dev:2");
 
     }
 }
