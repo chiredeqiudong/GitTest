bb5f36776ae806b33f56da162b3a8bfdbf4b4ef7
itzy <hello@itzy.cn>
2024-01-27 21:38:35 +0800

    master:4

--- src/main/java/com/it/pojo/Pojo.java
+++ src/main/java/com/it/pojo/Pojo.java
@@ -8,6 +8,7 @@ public class Pojo {
         System.out.println("hello git");
         System.out.println("master:1");
         System.out.println("master:2");
+        System.out.println("master:4");
 
     }
 }
