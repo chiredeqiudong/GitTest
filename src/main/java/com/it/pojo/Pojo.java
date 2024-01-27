d806457d0afa3155a85009aa027e541558f395c9
itzy <hello@itzy.cn>
2024-01-27 21:55:16 +0800

    master:7

--- src/main/java/com/it/pojo/Pojo.java
+++ src/main/java/com/it/pojo/Pojo.java
@@ -9,6 +9,6 @@ public class Pojo {
         System.out.println("master:1");
         System.out.println("master:2");
         System.out.println("master:6");
-
+        System.out.println("master:7");
     }
 }
